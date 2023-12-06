86a103e4281270e78fdee619d67c447e56135c98
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-06 13:37:30 -0800

    updated

--- BUDAPage/src/App.jsx
+++ BUDAPage/src/App.jsx
@@ -4,17 +4,19 @@ import PopperVid from "./components/PopperVid";
 import HipHopDisplay from "./components/PoppingDisplay";
 import SplitScreenContainer from "./containers/SplitScreenContainer";
 import { Home } from "./pages/Home";
+import "./styles/mainContainer.css";
 
 function App() {
   return (
     <div className="main-container">
       <Home />
+
       <SplitScreenContainer
-        StillComp={BreakDancerVid}
-        SlidableComp={BreakDanceDisplay}
-        isScrollRight={true}
-        bgColorBeg={"rgb(122, 200, 189)"}
-        bgColorEnd={"rgb(98, 166, 186)"}
+        SlidableComp={HipHopDisplay}
+        StillComp={PopperVid}
+        isScrollLeft={true}
+        bgColorBeg={"black"}
+        bgColorEnd={"gold"}
       />
     </div>
   );
--- BUDAPage/src/containers/SplitScreenContainer.jsx
+++ BUDAPage/src/containers/SplitScreenContainer.jsx
@@ -8,8 +8,6 @@ export default function SplitScreenContainer({
   bgColorEnd,
   bgColorBeg,
 }) {
-  console.log(isScrollLeft);
-  console.log(SlidableComp);
   return (
     <>
       {isScrollRight && (

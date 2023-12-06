51b596d36aac97a6ace2e1ad91deb6e44a588089
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-06 13:27:10 -0800

    updated

---
+++ BUDAPage/src/pages/Home.jsx
@@ -0,0 +1,47 @@
+import { useRef } from "react";
+import arrow from "../assets/arrowhead.png";
+import logo from "../assets/adsense.png";
+import techBg from "../assets/techBg.mp4";
+
+export function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-video-container">
+        <video
+          className="home-video"
+          src={techBg}
+          autoPlay
+          muted
+          loop
+          controls={false}
+        />
+      </div>
+      <div className="home-display-container">
+        <div className="home-display-logo-container">
+          <img src={logo} className="logo " />
+          <h1 className="my-3">Dance Technologies</h1>
+          <p className="my-3">Movement and Capture Development</p>
+        </div>
+
+        <div className="home-button-container">
+          <div className="home-left-buttons-container">
+            <button className="home-button">
+              Company Website <img src={arrow} width="10" />{" "}
+            </button>
+            <button className="home-button">
+              Founder's Podcast <img src={arrow} width="10" />
+            </button>
+          </div>
+          <div className="home-right-buttons-container">
+            <button className="home-button">
+              Company Portfolio <img src={arrow} width="10" />{" "}
+            </button>
+            <button className="home-button">
+              Live Project <img src={arrow} width="10" />{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

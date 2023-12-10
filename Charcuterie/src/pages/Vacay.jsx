7e77744dd39500a5857994585bef4cfa0f7cd17f
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-10 13:30:35 -0800

    updated

--- Charcuterie/src/pages/Vacay.jsx
+++ Charcuterie/src/pages/Vacay.jsx
@@ -6,7 +6,11 @@ export function Vacay() {
       <div className="vacay-bg-container">
         <img className="vacay-bg-img" src={vacay} />
       </div>
-      <div className="vacay-display"></div>
+      <div className="vacay-display">
+        <h3 className="vacay-display-text">WELCOME TO</h3>
+        <h1 className="vacay-display-text">The WellHall</h1>
+        <h2 className="vacay-display-text">Resort & Spa Hotel</h2>
+      </div>
     </div>
   );
 }

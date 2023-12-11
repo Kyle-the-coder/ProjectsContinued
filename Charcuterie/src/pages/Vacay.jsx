2649998383060084039f5950d9cdf482e0cc3e57
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-11 11:41:31 -0800

    updated

--- Charcuterie/src/pages/Vacay.jsx
+++ Charcuterie/src/pages/Vacay.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import vacay from "../assets/vacation.jpg";
 
 export function Vacay() {
+  const [firstLoad, setFirstLoad] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!firstLoad) {
+        setFirstLoad(true);
+      }
+    }, 1000);
+  }, [firstLoad]);
   return (
     <div className="vacay-container">
       <div className="vacay-bg-container">
         <img className="vacay-bg-img" src={vacay} />
       </div>
       <div className="vacay-display">
-        <h3 className="vacay-display-text">WELCOME TO</h3>
-        <h1 className="vacay-display-text">The WellHall</h1>
-        <h2 className="vacay-display-text">Resort & Spa Hotel</h2>
+        <h3 className={`vacay-display-text ${firstLoad && "active"}`}>
+          WELCOME TO
+        </h3>
+        <h1 className={`vacay-display-text ${firstLoad && "active"}`}>
+          The WellHall
+        </h1>
+        <h2 className={`vacay-display-text ${firstLoad && "active"}`}>
+          Resort & Spa Hotel
+        </h2>
+        <button className="vacay-button" onClick={() => setFirstLoad(false)}>
+          Reset
+        </button>
       </div>
     </div>
   );

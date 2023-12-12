c38ab088e2426afd0c652c4b3df7ae50ea9dd943
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-12 10:35:32 -0800

    updated

---
+++ Charcuterie/src/pages/Portfolio.jsx
@@ -0,0 +1,9 @@
+export function Portfolio() {
+  return (
+    <div className="portfolio-container">
+      <div className="portfolio-top"></div>
+      <div className="portfolio-bottom"></div>
+      <div className="portfolio-img-container"></div>
+    </div>
+  );
+}

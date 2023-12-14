53cb07b5bb66cf5992e08f4db30c7445fcb9d694
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-14 13:22:52 -0800

    updated

--- Charcuterie/src/pages/Shop.jsx
+++ Charcuterie/src/pages/Shop.jsx
@@ -1,12 +1,17 @@
-import fashion from "../assets/fashion.jpg";
+import fashion from "../assets/fashion3.jpg";
 export function Shop() {
   return (
     <div className="shop-main-container">
-      <div className="shop-left-container"></div>
+      <div className="shop-left-container">
+        <h1>This is fashoin!</h1>
+      </div>
       <div className="shop-right-container">
-        <img src={fashion} className="shop-right-container-img" />
-        <div className="ellipse ellipse1"></div>
-        <div className="ellipse ellipse2"></div>
+        <div className="shop-right-top-img-container">
+          <img src={fashion} className="shop-right-top-img" />
+        </div>
+        <div className="shop-right-bottom-img-container">
+          <img src={fashion} className="shop-right-bottom-img" />
+        </div>
       </div>
     </div>
   );

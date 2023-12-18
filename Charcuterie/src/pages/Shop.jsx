1bda701c0c14e0520fa4223db10c8a4aa3e99692
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-18 12:35:14 -0800

    updated

--- Charcuterie/src/pages/Shop.jsx
+++ Charcuterie/src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import fashion from "../assets/fashoin.jpeg";
+import logo from "../assets/cap2.png";
 import { gsap } from "gsap";
 import { useLayoutEffect } from "react";
 
@@ -8,7 +9,9 @@ export function Shop() {
       const t1 = gsap.timeline();
       t1.from(".shop-right-img", {
         xPercent: "-100",
-        duration: 1.3,
+        duration: 1.5,
+        delay: 1,
+        ease: "slow(0.9,2,false)",
       });
     });
     return () => ctx.revert();
@@ -16,7 +19,15 @@ export function Shop() {
   return (
     <div className="shop-main-container">
       <div className="shop-left-container">
-        <h1>This is fashoin!</h1>
+        <div className="shop-left-logo-container">
+          <img src={logo} className="shop-left-logo-png" />
+          <p className="shop-left-logo-title">Concrete Chic Boutique Co.</p>
+        </div>
+        <h1 className="shop-left-title">This is fashoin!</h1>
+        <div className="shop-left-clickable-container">
+          <button>Enter the Boutique</button>
+          <div></div>
+        </div>
       </div>
       <div className="shop-right-container">
         <img src={fashion} className="shop-right-img" />

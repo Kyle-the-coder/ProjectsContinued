e7e75b2ea43550952dd29ad8093a15eca34e76ef
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-18 11:54:29 -0800

    updated

--- Charcuterie/src/pages/Shop.jsx
+++ Charcuterie/src/pages/Shop.jsx
@@ -1,6 +1,18 @@
 import fashion from "../assets/fashoin.jpeg";
 import { gsap } from "gsap";
+import { useLayoutEffect } from "react";
+
 export function Shop() {
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const t1 = gsap.timeline();
+      t1.from(".shop-right-img", {
+        xPercent: "-100",
+        duration: 1.3,
+      });
+    });
+    return () => ctx.revert();
+  }, []);
   return (
     <div className="shop-main-container">
       <div className="shop-left-container">

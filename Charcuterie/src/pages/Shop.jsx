0318e142070cd7d78dae216559ca48b5bcd311db
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-18 13:00:08 -0800

    updated

--- Charcuterie/src/pages/Shop.jsx
+++ Charcuterie/src/pages/Shop.jsx
@@ -1,5 +1,8 @@
 import fashion from "../assets/fashoin.jpeg";
 import logo from "../assets/cap2.png";
+import insta from "../assets/instagram2.png";
+import tik from "../assets/tiktok.png";
+import fb from "../assets/facebook2.png";
 import { gsap } from "gsap";
 import { useLayoutEffect } from "react";
 
@@ -13,6 +16,21 @@ export function Shop() {
         delay: 1,
         ease: "slow(0.9,2,false)",
       });
+      t1.from(".shop-left-logo-container", {
+        yPercent: -100,
+        opacity: 0,
+        duration: 1.5,
+        delay: 1,
+      });
+      t1.from(
+        ".shop-left-clickable-container",
+        {
+          yPercent: 100,
+          opacity: 0,
+          duration: 1.5,
+        },
+        "-=1.5"
+      );
     });
     return () => ctx.revert();
   }, []);
@@ -25,8 +43,12 @@ export function Shop() {
         </div>
         <h1 className="shop-left-title">This is fashoin!</h1>
         <div className="shop-left-clickable-container">
-          <button>Enter the Boutique</button>
-          <div></div>
+          <button className="shop-left-button">Enter the Boutique</button>
+          <div className="shop-left-socials">
+            <img src={insta} className="shop-left-socials-png" />
+            <img src={tik} className="shop-left-socials-png" />
+            <img src={fb} className="shop-left-socials-png" />
+          </div>
         </div>
       </div>
       <div className="shop-right-container">

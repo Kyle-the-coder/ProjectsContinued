3d48db9a7d9a9710e314203215bd45ce5c9a66c7
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-21 11:02:58 -0800

    updated

--- Charcuterie/src/App.jsx
+++ Charcuterie/src/App.jsx
@@ -8,10 +8,29 @@ import { Vacay } from "./pages/Vacay";
 import "./styles/mainContainer.css";
 import { Portfolio } from "./pages/Portfolio";
 import { Shop } from "./pages/Shop";
+import { MouseTracker } from "./components/MouseTracker";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
   return (
     <div className="main-container">
+      <div
+        className="mouse-tracker"
+        style={{ left: mousePosition.x + "px", top: mousePosition.y + "px" }}
+      ></div>
       {/* <DotsContainer /> */}
 
       <Shop />

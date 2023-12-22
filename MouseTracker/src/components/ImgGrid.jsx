030539572bf404c466544b0173aee6aa23410086
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-22 10:40:49 -0800

    updated

--- MouseTracker/src/components/ImgGrid.jsx
+++ MouseTracker/src/components/ImgGrid.jsx
@@ -5,34 +5,33 @@ import gsap from "gsap";
 import "../styles/imggrid.css";
 
 const ImgGrid = () => {
-  const [images, setImages] = useState([]);
+  const [images1, setImages1] = useState([]);
+  const [images2, setImages2] = useState([]);
   const imgGridRef = useRef(null);
 
   useEffect(() => {
     const fetchImages = async () => {
       try {
-        // Your Pexels API key
         const apiKey =
           "q2nVuHy0uK8zolsVcRON0rK5pdZ7qvd6Crqtk7MGReVXQdaHl4nBcLQl";
 
-        // Pexels API endpoint for popular photos (you can adjust the endpoint based on your needs)
-        const apiUrl = "https://api.pexels.com/v1/curated";
+        const apiUrl = "https://api.pexels.com/v1/search";
 
-        // Axios request configuration
         const config = {
           headers: {
             Authorization: apiKey,
           },
           params: {
-            per_page: 9, // Adjust the number of images you want to fetch
+            query: "Tigers",
+            orientation: "portrait",
+            per_page: 3,
           },
         };
-
-        // Make a request to the Pexels API using axios
         const response = await axios.get(apiUrl, config);
 
-        // Update the state with the fetched images
-        setImages(response.data.photos);
+        setImages1(response.data.photos);
+
+        gsap.to(".img-grid", { duration: 5.2, yPercent: -150, delay: 1 });
       } catch (error) {
         console.error("Error fetching data from Pexels API:", error);
       }
@@ -41,35 +40,18 @@ const ImgGrid = () => {
     fetchImages();
   }, []); // Run the effect only once on component mount
 
-  useEffect(() => {
-    if (imgGridRef.current) {
-      const targets = Array.from({ length: 9 }, (_, i) => `#img-${i + 1}`).join(
-        ", "
-      );
-
-      const delayAnimation = () => {
-        gsap.from(targets, {
-          duration: 1,
-          stagger: 0.2,
-          opacity: 0,
-        });
-      };
-
-      // Use a timeout to delay the animation until after the images are loaded
-      setTimeout(delayAnimation, 1000);
-    }
-  }, [images]);
-
   return (
-    <div className="img-grid" ref={imgGridRef}>
-      {images.map((image, index) => (
-        <img
-          key={image.id}
-          id={`img-${index + 1}`}
-          src={image.src.medium}
-          alt={image.photographer}
-        />
-      ))}
+    <div className="img-grid-container" ref={imgGridRef}>
+      <div className="img-grid">
+        {images1.map((image, index) => (
+          <img
+            key={image.id}
+            id={`img-${index + 1}`}
+            src={image.src.medium}
+            alt={image.photographer}
+          />
+        ))}
+      </div>
     </div>
   );
 };

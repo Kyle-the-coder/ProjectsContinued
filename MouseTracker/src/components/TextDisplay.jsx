143896c8ac39ee3c863fb1a0de2512f61d5070a9
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-26 13:06:42 -0800

    updated

--- MouseTracker/src/components/TextDisplay.jsx
+++ MouseTracker/src/components/TextDisplay.jsx
@@ -1,8 +1,13 @@
+import logo from "../assets/camera.png";
 export function TextDisplay() {
   return (
     <div className="text-display-container">
-      <h1 className="text-display-text">Thomas WilderHall</h1>
-      <p>photographer, motion capture, artist</p>
+      <div className="text-display-logo-container">
+        <img src={logo} className="text-display-logo-png" />
+        <p className="m0">DreamLens Studios</p>
+      </div>
+      <h1 className="text-display-title">Thomas WilderHall</h1>
+      <p className="text-display-text">Photographer, Motion Capture, Artist</p>
     </div>
   );
 }

8b6b4f109cf91d2df81efca3f6859697a512b8b8
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-26 12:54:07 -0800

    updated

--- MouseTracker/src/components/TextDisplay.jsx
+++
@@ -1,8 +0,0 @@
-export function TextDisplay() {
-  return (
-    <div className="text-display-container">
-      <h1 className="text-display-text">Thomas WilderHall</h1>
-      <p>photographer, motion capture, artist</p>
-    </div>
-  );
-}

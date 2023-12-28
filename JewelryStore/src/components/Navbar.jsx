ae666cafd2fa405927b48f303d2d5f2757294721
Kyle Mitchell <kylethacoder@gmail.com>
2023-12-28 15:46:12 -0800

    updated

--- JewelryStore/src/components/Navbar.jsx
+++ JewelryStore/src/components/Navbar.jsx
@@ -1,30 +1,56 @@
 import "../styles/navbar.css";
+import logo from "../assets/j-store-logo.jpeg";
+import insta from "../assets/instagram2.png";
+import fb from "../assets/facebook2.png";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [hoverIndex, setHoverIndex] = useState(null);
+  const handleMouseEnter = (index) => {
+    setHoverIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverIndex(null);
+  };
+  const links = [
+    "Engagement Rings",
+    " Wedding Rings",
+    "Jewelry",
+    " Designers",
+    " Services",
+  ];
   return (
     <nav className="navbar-main-container">
       <div className="navbar-info-container">
         <div className="navbar-info-locations">
           <p>Santa Rosa</p>
+          <p>|</p>
           <p>Santa Clara</p>
         </div>
         <div className="navbar-info-logo">
-          <img />
+          <img src={logo} />
+          <p className="f2">Timothy Patrick Jewelers</p>
           <p>Est. 2009</p>
         </div>
         <div className="navbar-info-socials">
-          <img />
-          <img />
-          <img />
+          <img src={insta} />
+          <img src={fb} />
         </div>
       </div>
       <div className="navbar-links-container">
         <ul>
-          <li>Engagement Rings</li>
-          <li>Wedding Rings</li>
-          <li>Jewelry</li>
-          <li>Designers</li>
-          <li>Services</li>
+          {links.map((link, index) => {
+            return (
+              <li
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
+              >
+                {link}
+                {hoverIndex === index && <div className="active"></div>}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
